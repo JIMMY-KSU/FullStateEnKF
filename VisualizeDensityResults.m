a1fe7b75d1e9a79d67e25c494a629db4d7ed7b6c
Shaylah <shaylah.mutschler@colorado.edu>
2018-05-02 09:13:55 -0600

    Working RR EnSRF but need to make corrections
    Getting good results for q=39, 75 obs, all percent error under 160%
    After meeting w/ Penny determined corrections/changes to make to the filter. Commiting before making those updates

--- VisualizeDensityResults.m
+++ VisualizeDensityResults.m
@@ -1,7 +1,11 @@
 
 %% Read in and Prep Results
 
-load 'Data Files/7Period_Results.mat'; 
+ 
+load 'Data Files/RR_EnSRF_q39_75obs.mat';
+
+%load 'Data Files/7Period_Results_180RAAN.mat';
+
 
 % X_mean_updated_list_EnKF = mydict['X_mean_updated_list_EnKF'] 
 % P_list_EnKF = mydict['P_list_EnKF'] 
@@ -11,21 +15,38 @@ load 'Data Files/7Period_Results.mat';
 % X_distribution = mydict['X_distribution'] 
 % density_distribution = mydict['density_distribution'] 
 % lat_lst_array = mydict['lat_lst_array'] 
-% final_density_ensemble_est = mydict['final_density_ensemble_est']
+% final_density_ensem+ble_est = mydict['final_density_ensemble_est']
 % final_X_ensemble = mydict['final_X_ensemble']
-% 'true_density_array': true_density_array*1e9, 
-% 'final_density_grid_truth': final_density_grid_truth
+% 'true_density_array': true_density_array, 
+% 'final_density_grid_truth_timeSeries': final_density_grid_truth_timeSeries
 
 %final filter ensemble of density grids estimate 
 %average to get single grid
 final_density_grid_est = mean(final_density_ensemble_est, 1);
 final_density_grid_est = reshape(final_density_grid_est, 37, 73);
 
+stop_index = length(final_density_grid_truth_timeSeries);
+
 %final true density grid
-final_density_grid_truth = reshape(final_density_grid_truth, 37, 73);
+final_density_grid_truth = reshape(final_density_grid_truth_timeSeries(stop_index,:,:), 37, 73);
 
 
-error_sum = sum(sum(Z))
+%indices by station
+indices_1 = measurement_array(1:stop_index,2) == 1;
+indices_2 = measurement_array(1:stop_index,2) == 2;
+indices_3 = measurement_array(1:stop_index,2) == 3;
+indices_4 = measurement_array(1:stop_index,2) == 4;
+
+%% percent
+
+
+
+perc_error = 100 * abs(est_density_array - true_density_array(1:stop_index))./true_density_array(1:stop_index);
+
+figure;
+plot(measurement_array(1:stop_index,1), perc_error)
+
+
 
 
 %% Plot Final Time Results
@@ -33,58 +54,128 @@ error_sum = sum(sum(Z))
 x = -90:5:90;
 y = 0:5:360;
 [X,Y] = meshgrid(x,y);
-
-%difference between true and estimated density grid
-Z = final_density_grid_truth - final_density_grid_est;
-
-
-h = pcolor(Y,X,Z');
-set(h,'EdgeColor','none')
-%clabel(C, h)
-colorbar
-caxis([-2e-4,4e-4]); %force colorbar to be the same for each plot
-ylabel('Lat')
-xlabel('LST')
-zlabel('Density')
-title(['Density Estimate Error, Error Integral = ', num2str(error_sum)])%', longitude_grid(ii),'
+% 
+% %difference between true and estimated density grid
+% Z = final_density_grid_truth - final_density_grid_est;
+% Z = Z.^2;
+% error_sum = sum(sum(Z));
+% 
+% h = pcolor(Y,X,Z');
+% set(h,'EdgeColor','none')
+% %clabel(C, h)
+% colorbar
+% caxis([0,1e-6]); %force colorbar to be the same for each plot
+% ylabel('Lat')
+% xlabel('LST')
+% zlabel('Density')
+% title(['Density Estimate Error, Error Integral = ', num2str(error_sum)])%', longitude_grid(ii),'
 
 
 %% Process Time Series Results
 
-error_integral_array = zeros(length(est_density_grid_array),1);
-counter = 1;
 
-for ii = 1:25:length(est_density_grid_array)
+counter = 1;
+figure;
+title(['Density Estimate Error'])
+    
+for ii = 1:11:length(est_density_grid_array)
+    
+    ii
     
     %difference between true and estimated density grid
     Z = final_density_grid_truth_timeSeries(ii,:,:) - est_density_grid_array(ii,:,:);
     Z = reshape(Z, 37, 73);
-    Z = abs(Z);
+    Z = Z.^2;
 
-    error_integral_array(counter,1) = sum(sum(Z));  
-    
-    figure;
+
+    subplot(1,7,counter)
     h = pcolor(Y,X,Z');
     set(h,'EdgeColor','none')
     %clabel(C, h)
     colorbar
-    caxis([0,4e-4]); %force colorbar to be the same for each plot
+    caxis([0,1e-7]); %force colorbar to be the same for each plot
     ylabel('Lat')
     xlabel('LST')
     zlabel('Density')
-    title(['Density Estimate Error, Error Integral = ', num2str(error_integral_array(ii,1))])
+    title(['Time Step: ', num2str(ii)])
     
     counter = counter + 1;
     
 end
 
+%figure;  
+%plot(error_integral_array(2:end))
+
+
+
+%%
+error_integral_array = 1;% = zeros(length(est_density_grid_array),1);
+error_integral_array_sqrd = 1;
+
+for ii = 1:1:length(est_density_grid_array)
+    
+    %difference between true and estimated density grid
+    Z = final_density_grid_truth_timeSeries(ii,:,:) - est_density_grid_array(ii,:,:);
+    
+    error_integral_array = [error_integral_array; sum(sum(Z))];
+    
+    Z = Z.^2;
+    error_integral_array_sqrd = [error_integral_array_sqrd; sum(sum(Z))];
+    
+    
+end
+
+% figure;  
+% plot(error_integral_array(2:end))
+% ylabel('Error Integral')
+% xlabel('Time')
+% title(['Density Error Integral Over Grid Time Series (not abs or sqrd)'])
+
+error_integral_array_sqrd = error_integral_array_sqrd(2:end);
+
 figure;  
-plot(error_integral_array)
+hold;
+scatter(measurement_array(indices_1,1), error_integral_array_sqrd(indices_1),'filled', 's', 'MarkerFaceColor','m')
+scatter(measurement_array(indices_2,1), error_integral_array_sqrd(indices_2),'filled', '^', 'MarkerFaceColor',[0 0.5 0])
+scatter(measurement_array(indices_3,1), error_integral_array_sqrd(indices_3),'filled', 'd', 'MarkerFaceColor','r')
+scatter(measurement_array(indices_4,1), error_integral_array_sqrd(indices_4),'filled', 'o', 'MarkerFaceColor','k')
+ylabel('Error Integral')
+xlabel('Time')
+%legend({'Station 1','Station 2', 'Station 3', 'Station 4'})
+title(['Density Error Integral Over Grid Time Series '])
 
 
 
+%% Covariance Plots
+
+counter = 1;
+figure;
+
+x = 1:1:7;
+y = 1:1:7;
+[X,Y] = meshgrid(x,y);
 
 
+for ii = 1:10:length(est_density_grid_array)
+    
+    Z = P_list_EnKF(ii,:,:);
+    Z = reshape(Z, 7, 7);
+    
+    
+    subplot(1,7,counter)
+    h = pcolor(Y,X,Z');
+    set(h,'EdgeColor','none')
+    %clabel(C, h)
+    colorbar
+    caxis([0,1e-7]); %force colorbar to be the same for each plot
+    ylabel('State: XYZ Position, XYZ Velocity, Density')
+    xlabel('State')
+    zlabel('State Covariance')
+    title(['Time Step: ', num2str(ii)])
+    
+    counter = counter + 1;
+    
+end
 
 
 
--- plot_covariance.m
+++ plot_covariance.m
@@ -10,13 +10,13 @@ x = 1:1:2701;
 y = x;
 [X,Y] = meshgrid(x,y);
 
-Z = P_bar;
+Z = P_bar(7:2707,7:2707);
 
 h = pcolor(Y,X,Z');
 set(h,'EdgeColor','none')
 %clabel(C, h)
 colorbar
-caxis([0,.3]); %force colorbar to be the same for each plot
+caxis([.5e-9,1e-9]); %force colorbar to be the same for each plot
 ylabel('')
 xlabel('')
 zlabel('')
